cef5ccf97130d8db525d62df896dc48b5a2480c5
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    a5-common: Use threaded GLES render engine

--- system_prop.mk
+++ system_prop.mk
@@ -90,6 +90,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.logs=0 \
     debug.sf.disable_backpressure=1 \
     debug.sf.hw=1 \
+    debug.renderengine.backend=threaded \
     dev.pm.dyn_samplingrate=1 \
     persist.hwc.mdpcomp.enable=true \
     ro.sf.lcd_density=320 \
